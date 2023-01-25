6050b7e2ea6d7cfb32ccff6c7b48b04ad03df1e8
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-01-25 13:31:47 +0800

    ts inc selec

--- nvim/lua/ts.lua
+++ nvim/lua/ts.lua
@@ -7,6 +7,15 @@ return function(use)
                 highlight = {
                     enable = true,
                 },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = 'gnn',
+                        node_incremental = 'grn',
+                        scope_incremental = 'grc',
+                        node_decremental = 'grm',
+                    },
+                },
                 ensure_installed = {
                     'bash',
                     'c',
