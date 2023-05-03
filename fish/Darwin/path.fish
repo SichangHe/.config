6d87d7f77c872ae81fdeef5b51d5bd9f065c6fb0
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-05-03 21:42:41 +0800

    let LuaSnip find all.json;up python3.11

--- fish/Darwin/path.fish
+++ fish/Darwin/path.fish
@@ -1,6 +1,6 @@
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx NODE_PATH /opt/homebrew/lib/node_modules $NODE_PATH
-set -gx PYTHONPATH /opt/homebrew/lib/python3.10/site-packages $PYTHONPATH
+set -gx PYTHONPATH /opt/homebrew/lib/python3.11/site-packages $PYTHONPATH
 
 # postgres
 set -gx PATH /opt/homebrew/opt/libpq/bin $PATH
