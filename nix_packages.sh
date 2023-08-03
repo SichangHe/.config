6b051442eab57ba0fea93c3ef9a3e3e2fd855931
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-08-03 09:54:13 +0800

    git lfs

--- nix_packages.sh
+++ nix_packages.sh
@@ -18,6 +18,7 @@ nix-env -ibA nixpkgs.exa
 nix-env -ibA nixpkgs.exercism
 nix-env -ibA nixpkgs.fd
 nix-env -ibA nixpkgs.fzf
+nix-env -ibA nixpkgs.git-lfs
 nix-env -ibA nixpkgs.go
 nix-env -ibA nixpkgs.helix
 nix-env -ibA nixpkgs.jq
