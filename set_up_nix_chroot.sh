aeb30eb76517161b6a1f402b00d406bea8d6b543
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-01-27 07:33:34 +0800

    fix nix setup script

--- set_up_nix_chroot.sh
+++ set_up_nix_chroot.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir bin
-cd bin
+mkdir ~/bin
+cd ~/bin
 curl -L https://github.com/nix-community/nix-user-chroot/releases/download/1.2.2/nix-user-chroot-bin-1.2.2-x86_64-unknown-linux-musl \
     -o nix-user-chroot
 chmod +x nix-user-chroot
 
 mkdir -m 0755 ~/.nix
 ./nix-user-chroot ~/.nix bash -c "curl -L https://nixos.org/nix/install | bash"
-. /home/sh623/.nix-profile/etc/profile.d/nix.fish
-nix-env -ibA nixpkgs.fish
-alias n="~/bin/nix-user-chroot ~/.nix ~/.nix-profile/bin/fish"
+echo "nix-env -ibA nixpkgs.fish" | ~/bin/nix-user-chroot ~/.nix bash -l
+echo 'alias n="~/bin/nix-user-chroot ~/.nix ~/.nix-profile/bin/fish"' > ~/.profile
