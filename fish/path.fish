25b1e60a880cf254bd626c05f05717c57c5cf644
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-02-18 23:24:49 +0800

    mojo

--- fish/path.fish
+++ fish/path.fish
@@ -2,3 +2,4 @@ set -gx PATH /usr/local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.pub-cache/bin $PATH
 set -gx PATH ~/.local/share/gem/ruby/3.3.0+0/bin $PATH
+set -gx PATH ~/.modular/pkg/packages.modular.com_mojo/bin/ $PATH # mojo
