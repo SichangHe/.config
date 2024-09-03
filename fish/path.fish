0777244ea09332803c9dc7f9f0fe3b102e2d59a1
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-09-02 20:22:10 -0700

    clean up garbage installed via brew

--- fish/path.fish
+++ fish/path.fish
@@ -2,7 +2,6 @@ set -gx PATH /usr/local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.pub-cache/bin $PATH
 set -gx PATH ~/.local/share/gem/ruby/3.3.0+0/bin $PATH
-set -gx PATH ~/.modular/pkg/packages.modular.com_mojo/bin $PATH # mojo
 set -gx PATH ~/go/bin $PATH # Go-Lang
 set -gx PATH ~/.rye/shims $PATH # Rye for Python
 set -gx PATH ~/.dotnet/tools/ $PATH # .NET Tools
