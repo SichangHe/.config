241e1ee9d3d7893cce963a4c785b420a7630cedb
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2025-06-23 22:53:47 -0700

    mv to uv;btop w/ gpu support

--- fish/path.fish
+++ fish/path.fish
@@ -3,6 +3,5 @@ set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.pub-cache/bin $PATH
 set -gx PATH ~/.local/share/gem/ruby/3.3.0+0/bin $PATH
 set -gx PATH ~/go/bin $PATH # Go-Lang
-set -gx PATH ~/.rye/shims $PATH # Rye for Python
 set -gx PATH ~/.dotnet/tools/ $PATH # .NET Tools
 set -gx PATH "$PNPM_HOME" $PATH # PNPM
