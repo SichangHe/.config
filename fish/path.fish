e2a28c3b596b04a302b7634e2ccd9022f94b7ff4
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2026-01-06 11:59:00 -0800

    ruby gem path

--- fish/path.fish
+++ fish/path.fish
@@ -2,7 +2,9 @@ set -gx PATH /usr/local/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.pub-cache/bin $PATH
-set -gx PATH ~/.local/share/gem/ruby/3.3.0+0/bin $PATH
 set -gx PATH ~/go/bin $PATH # Go-Lang
 set -gx PATH ~/.dotnet/tools/ $PATH # .NET Tools
 set -gx PATH "$PNPM_HOME" $PATH # PNPM
+# Ruby gems
+set -gx GEM_HOME ~/gems
+set -gx PATH $GEM_HOME/bin $PATH
