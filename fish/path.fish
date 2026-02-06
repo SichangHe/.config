4c3876a0e5ad066a749c0bcd2e1e3cef6cb0a164
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2026-02-05 18:23:18 -0800

    oh my opencode;antigravity

--- fish/path.fish
+++ fish/path.fish
@@ -5,6 +5,7 @@ set -gx PATH ~/.pub-cache/bin $PATH
 set -gx PATH ~/go/bin $PATH # Go-Lang
 set -gx PATH ~/.dotnet/tools/ $PATH # .NET Tools
 set -gx PATH "$PNPM_HOME" $PATH # PNPM
+set -gx PATH ~/.opencode/bin $PATH # OpenCode
 # Ruby gems
 set -gx GEM_HOME ~/gems
 set -gx PATH $GEM_HOME/bin $PATH
